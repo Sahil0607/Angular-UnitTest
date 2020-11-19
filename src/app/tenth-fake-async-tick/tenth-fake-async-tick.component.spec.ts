0ab352b4b5edf3e61e180d1a8b2442d5f8964f72
Sahil0607 <patel.sahil331@gmail.com>
2020-11-18 19:11:00 -0600

    tenth commit

--- src/app/tenth-fake-async-tick/tenth-fake-async-tick.component.spec.ts
+++ src/app/tenth-fake-async-tick/tenth-fake-async-tick.component.spec.ts
@@ -4,15 +4,19 @@ import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core
 import { TenthFakeAsyncTickComponent } from './tenth-fake-async-tick.component';
 import { TenthFakeAsyncTickService } from './tenth-fake-async-tick.service';
 
+
+
 describe('TenthFakeAsyncTickComponent', () => {
   let component: TenthFakeAsyncTickComponent;
   let fixture: ComponentFixture<TenthFakeAsyncTickComponent>;
+
+  const TenthFakeAsyncTickServiceStub = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TenthFakeAsyncTickComponent ],
       imports: [ HttpClientModule ],
-      providers: [ TenthFakeAsyncTickService ]
+      providers: [ TenthFakeAsyncTickService ]  // { provide: TenthFakeAsyncTickService, useValue: TenthFakeAsyncTickServiceStub }
     })
     .compileComponents();
   }));
@@ -21,13 +25,46 @@ describe('TenthFakeAsyncTickComponent', () => {
     fixture = TestBed.createComponent(TenthFakeAsyncTickComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    // UserService from the root injector
+    // const tenthFakeAsyncTickService = TestBed.inject(TenthFakeAsyncTickService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should load todos from the server using async', async(() => {
+  it('should load todos from the server using async', async(() => {
+    const service: TenthFakeAsyncTickService = fixture.debugElement.injector.get(TenthFakeAsyncTickService);
+
+    // fixture.debugElement.injector.get(TodoTestService); // This is more verbal so use different approach
+    spyOn(service, 'getTodosPromise').and.returnValues(Promise.resolve([ [1, 2, 3] ]));
+    fixture.detectChanges();  // Angular call ngoninit and initialized todos property.
+    console.log('my service is ' + service);
+    fixture.whenStable().then(() => {    // Delay this line until async process completed.
+
+      expect(component.todos.length).toBe(3);
+    });
+  }));
+
+  // it('should load todos from the server using fakeAsyanc and tick', fakeAsync(() => {
+  //   const service = TestBed.get(TenthFakeAsyncTickService);
+
+  //   // fixture.debugElement.injector.get(TodoTestService); // This is more verbal so use different approach
+  //   spyOn(service, 'getTodosPromise').and.returnValues(Promise.resolve([ [1, 2, 3] ]));
+
+  //   fixture.detectChanges();  // Angular call ngoninit and initialized todos property.
+  //   tick(); // Delay this line until async process completed. Or simulate passage of time. Can use tick(1000); wait 1 sec
+  //   fixture.detectChanges();
+
+  //   expect(component.todos.length).toBe(3);
+  // }));
+});
+
+
+
+
+
+// it('should load todos from the server using async', async(() => {
   //   const service = TestBed.get(TenthFakeAsyncTickService);
 
   //   // fixture.debugElement.injector.get(TodoTestService); // This is more verbal so use different approach
@@ -39,14 +76,14 @@ describe('TenthFakeAsyncTickComponent', () => {
   //   });
   // }));
 
-  it('should load todos from the server using fakeAsyanc and tick', fakeAsync(() => {
-    const service = TestBed.get(TenthFakeAsyncTickService);
+  // it('should load todos from the server using fakeAsyanc and tick', fakeAsync(() => {
+  //   const service = TestBed.get(TenthFakeAsyncTickService);
 
-    // fixture.debugElement.injector.get(TodoTestService); // This is more verbal so use different approach
-    spyOn(service, 'getTodosPromise').and.returnValues(Promise.resolve([ [1, 2, 3] ]));
-    fixture.detectChanges();  // Angular call ngoninit and initialized todos property.
+  //   // fixture.debugElement.injector.get(TodoTestService); // This is more verbal so use different approach
+  //   spyOn(service, 'getTodosPromise').and.returnValues(Promise.resolve([ [1, 2, 3] ]));
 
-    tick(); // Delay this line until async process completed. Or simulate passage of time. Can use tick(1000); wait 1 sec
-    expect(component.todos.length).toBe(3);
-  }));
-});
+  //   fixture.detectChanges();  // Angular call ngoninit and initialized todos property.
+  //   tick(); // Delay this line until async process completed. Or simulate passage of time. Can use tick(1000); wait 1 sec
+  //   fixture.detectChanges();
+
+  //   expect(component.todos.length).toBe(3);
